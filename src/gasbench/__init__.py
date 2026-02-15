468560698b420749f792b12eb46942a733abf4e8
dylan <dylan@bitmind.ai>
2026-02-15 18:35:00 +0000

    bump ver

--- src/gasbench/__init__.py
+++ src/gasbench/__init__.py
@@ -20,7 +20,7 @@ from .benchmarks.video_bench import run_video_benchmark
 from .benchmark import run_benchmark, print_benchmark_summary, format_benchmark_summary, save_results_to_json
 from .benchmarks.utils.inference import create_inference_session
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __author__ = "bmbm"
 
 __all__ = [
